501767388b137661a0f2a29337b0a76d51ec3da7
Benjamin Benoist <benjamin.benoist@snowplowanalytics.com>
2019-03-18 23:18:16 +0100

    do not use parseOption, that swallows the exception

--- 3-enrich/scala-common-enrich/src/main/scala/com.snowplowanalytics.snowplow.enrich/common/utils/ConversionUtils.scala
+++ 3-enrich/scala-common-enrich/src/main/scala/com.snowplowanalytics.snowplow.enrich/common/utils/ConversionUtils.scala
@@ -43,6 +43,7 @@ import io.lemonlabs.uri.Uri
 import io.lemonlabs.uri.Url
 import io.lemonlabs.uri.config.UriConfig
 import io.lemonlabs.uri.decoding.PercentDecoder
+import io.lemonlabs.uri.encoding.percentEncode
 
 /**
  * General-purpose utils to help the
@@ -314,13 +315,6 @@ object ConversionUtils {
    * Parses a string to create a [[URI]].
    * Parsing is relaxed, i.e. even if a URL is not correctly percent-encoded or not RFC 3986-compliant, it can be parsed.
    *
-   * First, parsing is tried with [[URI.create]] and if it does not succeed,
-   * a more relaxed parsing is done with scala-uri.
-   * The reason why not using scala-uri directly is that it can encode input valid URIs.
-   * For example, with scala-uri :
-   * - http://example.com/pa+th?param=val+ue becomes http://example.com/pa%20th?param=val+ue
-   * - http://example.com/qs?param=a%20b becomes http://example.com/qs?param=a%2520b
-   *
    * @param uri String containing the URI to parse.
    * @return [[Validation]] wrapping the result of the parsing:
    *         - [[Success]] with the parsed URI if there was no error or with [[None]] if the input was `null`.
@@ -328,17 +322,25 @@ object ConversionUtils {
    */
   def stringToUri(uri: String): Validation[String, Option[URI]] =
     try {
-      Option(uri).map(_.replaceAll(" ", "%20")).map(URI.create).success
+      Option(uri) // to handle null
+        .map(_.replaceAll(" ", "%20"))
+        .map(URI.create)
+        .success
     } catch {
-      case NonFatal(e) =>
+      case NonFatal(javaErr) =>
         try {
-          implicit val c = UriConfig(decoder = PercentDecoder(ignoreInvalidPercentEncoding = true))
-          Uri.parseOption(uri).map(_.toJavaURI).success
+          implicit val c =
+            UriConfig(decoder = PercentDecoder(ignoreInvalidPercentEncoding = true), encoder = percentEncode -- '+')
+          Uri
+            .parse(uri)
+            .toJavaURI
+            .some
+            .success
         } catch {
-          case NonFatal(e) =>
-            "Provided URI [%s] could not be parsed. Error: [%s]"
-              .format(uri, e.getMessage)
-              .fail
+          case NonFatal(scalaErr) =>
+            "Provided URI [%s] could not be parsed, neither by Java parsing (error: [%s]) nor by Scala parsing (error: [%s])."
+              .format(uri, javaErr.getMessage, scalaErr.getMessage)
+              .failure
         }
     }
 
